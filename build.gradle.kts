69c51efa0bc24e2015bb2d07df012cf5427cb4ca
Ridwan <149461171+RIdwan1133@users.noreply.github.com>
2025-06-09 13:15:49 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.iris_kotlin_221351084"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.iris_kotlin_221351084"
